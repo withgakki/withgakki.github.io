86f06477d426586fa81c8c7fd80f1feeaf2d6e7b
mldn <1059041079@qq.com>
2019-05-22 14:31:01 +0800

    Update music.js

--- js/dist/music.js
+++ js/dist/music.js
@@ -1,13 +1,14 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    fixed: true,
+    // fixed: true,
+    mini: true,
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "像鱼",
+        artist: '王贰浪',
+        url: 'http://www.ytmp3.cn/down/56782.mp3',
+        cover: 'http://p1.music.126.net/ejEPGN6ulPSgCBXGq7dgqw==/109951163720047382.jpg?param=130y130',
       }
     ]
 });
