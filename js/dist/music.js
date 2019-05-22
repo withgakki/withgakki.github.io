593c508340a38f8125b9d5c091615ee2a6015b06
mldn <1059041079@qq.com>
2019-05-22 14:49:24 +0800

    5-22

--- js/dist/music.js
+++ js/dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    // fixed: true,
-    mini: true,
+    fixed: true,
+    // mini: true,
     autoplay: false,
     audio: [
       {
@@ -9,6 +9,18 @@ const ap = new APlayer({
         artist: '王贰浪',
         url: 'http://www.ytmp3.cn/down/56782.mp3',
         cover: 'http://p1.music.126.net/ejEPGN6ulPSgCBXGq7dgqw==/109951163720047382.jpg?param=130y130',
+      },
+      {
+        name: "天空之城",
+        artist: '李志',
+        url: 'http://www.ytmp3.cn/down/47726.mp3',
+        cover: 'http://p2.music.126.net/xIonZ9I4YtnljJmQzv32hA==/109951163214060584.jpg?param=130y130',
+      },
+      {
+        name: "借我",
+        artist: '谢春花',
+        url: 'http://www.ytmp3.cn/down/34511.mp3',
+        cover: 'http://p2.music.126.net/Eg4cy0_HIF2nrX2gMCsWkQ==/17967119509636556.jpg?param=130y130',
       }
     ]
 });
