c49d1b1633e8fa5b0211538b8231ae755fd5fefc
mldn <1059041079@qq.com>
2019-05-22 11:45:50 +0800

    music

---
+++ js/dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      }
+    ]
+});
